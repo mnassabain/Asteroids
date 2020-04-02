2118ac38b6569214b97f172e9425826a499bd863
MARCO NASSABAIN <marco.nassabain@hotmail.com>
2020-04-02 21:33:07 +0200

    speed up spaceship

--- src/Spaceship.cpp
+++ src/Spaceship.cpp
@@ -109,23 +109,23 @@ void Spaceship::move()
     double alpha = (orientation % 90) * M_PI / 180;
     if (0 <= orientation && orientation < 90)
     {
-        dx = sin(alpha) * speed * 1.7;
-        dy = -cos(alpha) * speed * 1.7;
+        dx = sin(alpha) * speed * 2;
+        dy = -cos(alpha) * speed * 2;
     }
     else if (90 <= orientation && orientation < 180)
     {
-        dx = cos(alpha) * speed * 1.7;
-        dy = sin(alpha) * speed * 1.7;
+        dx = cos(alpha) * speed * 2;
+        dy = sin(alpha) * speed * 2;
     }
     else if (180 <= orientation && orientation < 270)
     {
-        dx = -sin(alpha) * speed * 1.7;
-        dy = cos(alpha) * speed * 1.7;
+        dx = -sin(alpha) * speed * 2;
+        dy = cos(alpha) * speed * 2;
     }
     else
     {
-        dx = -cos(alpha) * speed * 1.7;
-        dy = -sin(alpha) * speed * 1.7;
+        dx = -cos(alpha) * speed * 2;
+        dy = -sin(alpha) * speed * 2;
     }
 
     setPosition(getPosition() + Vect2D(dx, dy));
