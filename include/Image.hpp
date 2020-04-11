1ce93efb01a35f763fa88134b0008dab056b21eb
MARCO NASSABAIN <marco.nassabain@hotmail.com>
2020-04-11 18:50:38 +0200

    implemented possibility to extract surface from texture

--- include/Image.hpp
+++ include/Image.hpp
@@ -9,13 +9,15 @@ class Image
 {
     private:
         SDL_Texture* texture;
-        Rect box;
+        Rect surface;       // surface on texture
+        Rect box;           // where it needs to be drawn
     
     public:
         static const int TITLE_IMAGE_WIDTH = 382;
         static const int TITLE_IMAGE_HEIGHT = 32;
 
         Image(SDL_Texture*, Rect&);
+        Image(SDL_Texture*, Rect&, Rect&);
         void draw();
 };
 
--- src/Image.cpp
+++ src/Image.cpp
@@ -1,13 +1,29 @@
 #include <Image.hpp>
 
-Image::Image(SDL_Texture* t, Rect& b)
+Image::Image(SDL_Texture* t, Rect& b) : surface(0, 0, -1, -1)
 {
     texture = t;
     box = b;
 }
 
+Image::Image(SDL_Texture* t, Rect& b, Rect& s)
+{
+    texture = t;
+    box = b;
+    surface = s;
+}
+
 void Image::draw()
 {
     SDL_Rect dest = { box.x(), box.y(), box.w(), box.h() };
-    Engine::draw(texture, NULL, &dest, 0);
+    if (surface.w() == -1)
+    {
+        Engine::draw(texture, NULL, &dest, 0);
+    }
+    else
+    {
+        SDL_Rect surf = { surface.x(), surface.y(), surface.w(), surface.h() };
+        Engine::draw(texture, &surf, &dest, 0);
+    }
+    
 }
